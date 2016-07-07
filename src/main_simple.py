81acc6aee01cb5a3de86fabbef5d1f42bb9be9e6
Jose Luis Martin-Javato <joseluismartinjavato@MacBook-de-Jose-Luis-Martin-Javato.local>
2016-07-07 09:50:19 +0200

    v.0.7.0

--- src/main_simple.py
+++ src/main_simple.py
@@ -8,13 +8,13 @@ separar = Separacion.Separacion()
 filtro = Filtros.Filtros()
 
 #img = cv2.imread('../met_1_vec_0_sig_-1_thr_180_binImg.png', 0)
-img = cv2.imread('../met_1_vec_0_sig_0_thr_143_binImg.png', 0)
-img2 = cv2.imread('../imgs/Narciso1.png', -1)
+img = cv2.imread('../met_1_vec_0_sig_0_thr_134_binImg.png', 0)
+img2 = cv2.imread('../imgs/Narciso2.png', -1)
 
 filas, colum = img.shape
 
 print("Histograma horizontal")
-#hist_hor = separar.hor_hist(img)
+#ist_hor = separar.hor_hist(img)
 
 print("Histograma vertical")
 hist_ver = separar.vert_hist(img)
@@ -27,8 +27,8 @@ ini_filas, fin_filas = separar.filas(filtrado, 20)
 tam = len(ini_filas)
 
 for x in range(0,tam):
-    cv2.line(img2, (0, ini_filas[x]), (colum, ini_filas[x]), 100, 1)
-    cv2.line(img2, (0, fin_filas[x]), (colum, fin_filas[x]), 100, 1)
+    cv2.line(img2, (0, ini_filas[x]), (colum, ini_filas[x]), 100, 5)
+    cv2.line(img2, (0, fin_filas[x]), (colum, fin_filas[x]), 100, 5)
 
 print("Separar palabras")
 for x in range(0,tam):
@@ -38,8 +38,8 @@ for x in range(0,tam):
 
     tam_palabra = len(ini_palabra)
     for y in range(0,tam_palabra):
-        cv2.line(img2, (ini_palabra[y], ini_filas[x]), (ini_palabra[y], fin_filas[x]), 100, 1)
-        cv2.line(img2, (fin_palabra[y], ini_filas[x]), (fin_palabra[y], fin_filas[x]), 100, 1)
+        cv2.line(img2, (ini_palabra[y], ini_filas[x]), (ini_palabra[y], fin_filas[x]), 100, 3)
+        cv2.line(img2, (fin_palabra[y], ini_filas[x]), (fin_palabra[y], fin_filas[x]), 100, 3)
 
 
 #cv2.namedWindow('result', cv2.WINDOW_AUTOSIZE)
@@ -49,7 +49,7 @@ cv2.imwrite('../salida1.png', img2)
 print("Resultados gráficos")
 plt.figure(1)
 #plt.subplot(211)
-plt.plot(hist_hor)
+plt.plot(hist_ver)
 #plt.plot(ini_palabra, np.zeros(tam_palabra), 'ro')
 #plt.plot(fin_palabra, np.zeros(tam_palabra), 'bo')
 plt.show()
