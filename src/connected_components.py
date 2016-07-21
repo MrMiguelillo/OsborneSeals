8104b6b7aa20d1db83fb7e4ef45fcdc8a1f7c0c2
Miguel A.R <miguelrodrigo249@gmail.com>
2016-07-21 13:01:44 +0200

    Comments updated

--- src/connected_components.py
+++ src/connected_components.py
@@ -5,7 +5,7 @@ import matplotlib.patches as mpatches
 
 img = cv2.imread('C:/Users/usuario/Documents/Lab_Osborne/Fotos_sellos/2.png', cv2.IMREAD_GRAYSCALE)
 bin_img = (img < 180).astype('uint8')*255
-#    fila_ero_bw = img_ero_bw[ini_filas[y]:fin_filas[y], 0:col_px]
+
 label_image = measure.label(bin_img)
 
 for region in measure.regionprops(label_image):
@@ -13,12 +13,13 @@ for region in measure.regionprops(label_image):
     if region.area < 1000:
         continue
 
-    # draw rectangle around segmented coins
+    # check for filled area ratio inside bounding box
     minr, minc, maxr, maxc = region.bbox
     fill_area_ratio = float(region.area) / ((maxr - minr)*(maxc - minc))
-    if fill_area_ratio < 0.3 or fill_area_ratio > 0.9:  #0.9 condition just to avoid black margins false positives
+    if fill_area_ratio < 0.3 or fill_area_ratio > 0.9:  # 0.9 condition just to avoid black margins false positives
         continue
 
+    # draw rectangle around detected regions
     ratio_string = '%f' % fill_area_ratio
     cent_row, cent_col = region.centroid
     cv2.putText(bin_img, ratio_string, (int(cent_col), int(cent_row)), cv2.FONT_HERSHEY_COMPLEX, 1, 180, 5)
