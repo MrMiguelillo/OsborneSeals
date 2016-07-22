f1ebcea9e051650c6591ffff7a22b29aec8898a7
Miguel A.R <miguelrodrigo249@gmail.com>
2016-07-22 13:36:15 +0200

    image link generalized for collaborative usage purposes

--- src/connected_components.py
+++ src/connected_components.py
@@ -3,7 +3,7 @@ import numpy as np
 from skimage import measure
 import matplotlib.patches as mpatches
 
-img = cv2.imread('C:/Users/usuario/Documents/Lab_Osborne/Fotos_sellos/2.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('../imgs/IMG_0001.png', cv2.IMREAD_GRAYSCALE)
 bin_img = (img < 180).astype('uint8')*255
 
 label_image = measure.label(bin_img)
@@ -19,18 +19,16 @@ for region in measure.regionprops(label_image):
     if fill_area_ratio < 0.3 or fill_area_ratio > 0.9:  # 0.9 condition just to avoid black margins false positives
         continue
     else:
-        print('%d %d %d %d' % (minc, minr, maxc, maxr))
-        cv2.namedWindow('test', cv2.WINDOW_NORMAL)
-        cv2.imshow('test', bin_img[minc:int((maxc+minc)/2), minr:maxr])
-        cv2.waitKey()
-        cv2.destroyWindow('test')
+        #print('%d %d %d %d' % (minc, minr, maxc, maxr))
+        #cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+        #cv2.imshow('test', bin_img[minc:int((maxc+minc)/2), minr:maxr])
+        #cv2.waitKey()
+        #cv2.destroyWindow('test')
         left_half_area = np.sum(bin_img[minc:int((maxc+minc)/2), minr:maxr])
         right_half_area = np.sum(bin_img[int((maxc+minc)/2)+1:maxc, minr:maxr])  # Coger slice de mitad derecha
-        #top_half_area = np.sum(bin_img[minc:maxc, minr:(maxr+minr)/2])
-        #bot_half_area = np.sum(bin_img[minc:maxc, (maxr+minr)/2 + 1:maxr])
         half_area_ratio = left_half_area/right_half_area
-        #if half_area_ratio < 0.97 or half_area_ratio > (1/0.97):
-         #   continue
+        if half_area_ratio < 0.9 or half_area_ratio > (1/0.9):
+            continue
 
     # draw rectangle around detected regions
     ratio_string = '%f' % half_area_ratio
