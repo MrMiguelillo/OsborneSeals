a011a87be5dc001c2ab3fd77c0b545a241752165
Miguel A.R <miguelrodrigo249@gmail.com>
2016-09-07 12:32:48 +0200

    Limpiado de comentarios, funcionalidad intacta

--- src/EvidenceMatrix.py
+++ src/EvidenceMatrix.py
@@ -3,23 +3,23 @@ import cv2
 
 
 class SealLocator:
-    NUM_DIVISSIONS = 10
+    DIVISION_SIZE = 10
     SEAL_DIMENSION = 300  # seals are about 300x300px
     evidence_matrix = np.array([[]])
 
     def __init__(self, img):
         fils, cols = img.shape
-        self.evidence_matrix.resize((fils / SealLocator.NUM_DIVISSIONS, cols / SealLocator.NUM_DIVISSIONS))
+        self.evidence_matrix.resize((fils / SealLocator.DIVISION_SIZE, cols / SealLocator.DIVISION_SIZE))
         self.evidence_matrix.fill(0)
 
     def calc_occurrences(self, keypoints):
         for kp in keypoints:
             # points are x, y instead of rows and columns
-            normal = (int(kp.pt[1] / SealLocator.NUM_DIVISSIONS), int(kp.pt[0] / SealLocator.NUM_DIVISSIONS))
+            normal = (int(kp.pt[1] / SealLocator.DIVISION_SIZE), int(kp.pt[0] / SealLocator.DIVISION_SIZE))
             self.evidence_matrix[normal[0], normal[1]] += 1
 
     def calc_position(self):
-        num_cells = int(self.SEAL_DIMENSION / self.NUM_DIVISSIONS)
+        num_cells = int(self.SEAL_DIMENSION / self.DIVISION_SIZE)
         kernel = np.ones((num_cells, num_cells))
 
         occurrences = cv2.filter2D(self.evidence_matrix, -1, kernel)
--- src/test_convolucion.py
+++ src/test_convolucion.py
@@ -3,19 +3,19 @@ import cv2
 import cv2.xfeatures2d as xf
 import src.EvidenceMatrix as em
 
-img1 = cv2.imread('C:/Users/usuario/Desktop/Base_sellos/sello6.png', 0)    # trainImage
-img2 = cv2.imread('C:/Users/usuario/Desktop/documentos/1882-L123.M17/1/1882-L123.M17.I-1/IMG_0002.png', 0)  # queryImage
+img1 = cv2.imread('C:/Users/usuario/Desktop/Base_sellos/sello6.png', 0)    # queryImage
+img2 = cv2.imread('C:/Users/usuario/Desktop/documentos/1882-L123.M17/1/1882-L123.M17.I-1/IMG_0002.png', 0)  # trainImage
 
 
 # --------------- DETECTION ---------------
 
 
 # Initiate SURF detector
-sift = xf.SURF_create()
+surf = xf.SURF_create()
 
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+kp1, des1 = surf.detectAndCompute(img1, None)
+kp2, des2 = surf.detectAndCompute(img2, None)
 
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
@@ -35,7 +35,7 @@ kp_matched = []
 for i, (m, n) in enumerate(matches):
     if m.distance < 0.9*n.distance:
         matchesMask[i] = [1, 0]
-        kp_matched.append(kp2[m.trainIdx])  # keypoints from query image stored for seal location calculation
+        kp_matched.append(kp2[m.trainIdx])  # keypoints from train image stored for seal location calculation
 
 draw_params = dict(matchColor=(0, 255, 0),
                    singlePointColor=(255, 0, 0),
@@ -44,13 +44,10 @@ draw_params = dict(matchColor=(0, 255, 0),
 
 
 img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
-# print(len(kp_matched))
 
 
 cv2.namedWindow('win', cv2.WINDOW_NORMAL)
 cv2.imshow('win', img3)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
 
 
 # --------------- OUTLIERS ELIMINATION ---------------
@@ -58,36 +55,17 @@ cv2.imshow('win', img3)
 seal_locator = em.SealLocator(img2)
 seal_locator.calc_occurrences(kp_matched)
 y, x = seal_locator.calc_position()
-num_div = seal_locator.NUM_DIVISSIONS
-
-print(y)
-print(x)
+num_div = seal_locator.DIVISION_SIZE
 
 kpMask = [1 for i in range(len(kp_matched))]
 
 img4 = img2.copy()
 img4 = cv2.cvtColor(img4, cv2.COLOR_GRAY2BGR)
 
-# for kp in kp_matched:
-#     cv2.circle(img4, (int(kp.pt[0]), int(kp.pt[1])), 10, (255, 0, 255), -1)
-
 cv2.circle(img4, (x*num_div, y*num_div), 15, (255, 0, 255), thickness=-1)
-# for i, kp in enumerate(kp_matched):
-#     if abs(kp.pt[0] - y*num_div) <= 30*num_div and abs(kp.pt[1] - x*num_div) <= 30*num_div:
-#         print(kp.pt)
-#         kpMask[i] = 0
-#         cv2.circle(img4, (int(kp.pt[0]), int(kp.pt[1])), 7, (255, 0, 255), thickness=-1)
 
-# draw_params = dict(matchColor=(0, 255, 0),
-#                    singlePointColor=(255, 0, 0),
-#                    matchesMask=matchesMask,
-#                    flags=0)
-#
-# img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
-#
 cv2.namedWindow('win2', cv2.WINDOW_NORMAL)
 cv2.imshow('win2', img4)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-# TODO: Dibujar kp_matched para ver por qué coño se detecta el punto allí
