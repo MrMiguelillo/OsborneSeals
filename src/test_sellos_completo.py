cc51ff54100de34fca46c2cd07754ef033d32b59
Miguel A.R <miguelrodrigo249@gmail.com>
2016-09-22 12:44:21 +0200

    eliminacion y deteccion de sellos funciona

--- src/test_sellos_completo.py
+++ src/test_sellos_completo.py
@@ -1,9 +1,11 @@
 import src.EliminacionSellos as ElimSe
-import src.EvidenceMatrix as em
 import cv2
 
 
-img = cv2.imread('C:/Users/usuario/Desktop/documentos/1882-L123.M17/1/1882-L123.M17.I-1/IMG_0002.png', 0)  # trainImage
+img = cv2.imread('C:/Users/usuario/Desktop/documentos/1877-L119.M23_Tomas_Osborne_Bohl/3/'
+                 '1877-L119.M23_TomasOsborneBohl.I_3/IMG_0001.png', 0)
+# img = cv2.imread('C:/Users/usuario/Desktop/documentos/1882-L123.M17/
+# 1/1882-L123.M17.I-1/IMG_0002.png', 0)  # trainImage
 
 path = 'C:/Users/usuario/Desktop/Base_sellos/'
 elim_sellos = []
@@ -18,6 +20,7 @@ real_seal = -1
 max_occurrences = 0
 for i in range(0, 5):
     elim_sellos[i].get_matched_keypoints()
+    elim_sellos[i].compute_evidence_matrix()
     elim_sellos[i].compute_position_and_max_occurrences()
     if elim_sellos[i].max_occurrences > max_occurrences:
         max_occurrences = elim_sellos[i].max_occurrences
@@ -25,18 +28,8 @@ for i in range(0, 5):
 
 elim_sellos[real_seal].remove_seal()
 
-colors = [(255, 20, 255),
-          (20, 255, 30),
-          (10, 10, 10),
-          (10, 20, 255),
-          (200, 10, 40)]
-
-# for i, el_se in enumerate(elim_sellos):
-#     el_se.calc_occurrences(el_se.kp_matched)
-#     print(el_se[i].is_this_seal_type())
-#     cv2.circle(doc_img, elim_sellos.vect[])
-
 cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
 cv2.imshow('Result', img)
+cv2.waitKey()
 
 # TODO: Funciona, falta ver si devuelve el resultado correcto
