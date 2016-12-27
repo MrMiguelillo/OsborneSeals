daee0207ed83c25231b45a1ae6539be240ae61eb
Miguel A.R <miguelrodrigo249@gmail.com>
2016-12-27 17:05:49 +0100

    detectar_sellos_test now works recursively

--- detectar_sellos_test.py
+++ detectar_sellos_test.py
@@ -1,8 +1,29 @@
 import cv2
-import numpy as np
+import os
 from FuncionSellosCompleta import detectar_sello
 
-img = cv2.imread('imgs/10_IMG_0003.png', 0)
-img2, coords, nombre = detectar_sello(img)
+path = 'C:/Users/usuario/Desktop/document'
+walk = os.walk(path)
 
-print(coords)
+for root, dirs, files in walk:
+    print(root)
+    max_points = 0
+    curr_name = ''
+    max_coords = (0, 0)
+    there_is_any_image = False
+    for curr_file in files:
+        if curr_file.endswith('.png'):
+            there_is_any_image = True
+            img = cv2.imread(root + '/' + curr_file, 0)
+            img2, coords, nombre, points = detectar_sello(img)
+            if points > max_points:
+                max_points = points
+                curr_name = nombre
+                max_coords = coords
+
+    if there_is_any_image:
+        txt_file = open(root + '/result.txt', 'w')
+        txt_file.write(curr_name + '\n')
+        txt_file.write(str(max_coords) + '\n')
+        txt_file.write(str(max_points) + '\n')
+        txt_file.close()
