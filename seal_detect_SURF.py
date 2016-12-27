10de5c477b1b430d3e94593da2984bde914b4408
Miguel A.R <miguelrodrigo249@gmail.com>
2016-12-27 17:08:00 +0100

    some code commented out. Minor changes just for tests

--- seal_detect_SURF.py
+++ seal_detect_SURF.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2.xfeatures2d as xf
 import cv2
 
-img1 = cv2.imread('C:/Users/usuario/Desktop/Base_sellos/sello0.png', 0)    # trainImage
+img1 = cv2.imread('C:/Users/usuario/Desktop/Base_sellos/sello90.png', 0)    # trainImage
 # img2 = cv2.imread('C:/Users/usuario/Desktop/documentos/1877-L119.M23_Tomas_Osborne_Bohl/'
 #                   '1/1877-L119.M23_Tomas_Osborne_Bohl.I_1/IMG_0001.png', 0)  # queryImage
 
@@ -32,7 +32,7 @@ kp_matched = []
 for i, (m, n) in enumerate(matches):
     if True:  # m.distance < 0.9*n.distance:
         matchesMask[i] = [1, 0]
-        kp_matched.append(kp2[m.queryIdx])  # keypoints from query image stored for seal location calculation
+        kp_matched.append(kp2[m.trainIdx])  # keypoints from query image stored for seal location calculation
 
 draw_params = dict(matchColor=(0, 255, 0),
                    singlePointColor=(255, 0, 0),
@@ -51,11 +51,22 @@ draw_params = dict(matchColor=(0, 255, 0),
 
 # cv2.rectangle(img2, (int(min_x),int(min_y)), (int(max_x),int(max_y)), 150, 5)
 
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
-print(len(kp_matched))
+# img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+# img3 = img2.copy()
+# img3 = cv2.cvtColor(img3, cv2.COLOR_GRAY2BGR)
+# for i in range(0, len(kp_matched)):
+#     cv2.circle(img3, (int(kp_matched[i].pt[0]), int(kp_matched[i].pt[1])), 6, (0, 255, 0), thickness=-1)
+#
+# fils, cols = img2.shape
+# for i in range(0, int(cols/100)):
+#     cv2.line(img3, (100*i, 0), (100*i, fils), (255, 0, 50), thickness=5)
+# for i in range(0, int(fils/100)):
+#     cv2.line(img3, (0, 100*i), (cols, 100*i), (255, 0, 50), thickness=5)
+# cv2.drawKeypoints(img2, kp_matched, img3, color=(0, 255, 0))
+# print(len(kp_matched))
 
 
 cv2.namedWindow('win', cv2.WINDOW_NORMAL)
-cv2.imshow('win', img3)
+cv2.imshow('win', img2)
 cv2.waitKey()
 cv2.destroyAllWindows()
