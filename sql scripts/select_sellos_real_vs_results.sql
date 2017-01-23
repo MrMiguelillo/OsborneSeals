f28c67fe357f9300568664c55fa0fff0bf3de03e
Miguel A.R <miguelrodrigo249@gmail.com>
2017-01-23 19:21:13 +0100

    results query now shows route and ratio as well for debugging purposes

--- sql scripts/select_sellos_real_vs_results.sql
+++ sql scripts/select_sellos_real_vs_results.sql
@@ -1,3 +1,3 @@
-select docs_osborne.documentos.sello, docs_osborne.results.sello
+select docs_osborne.documentos.sello, docs_osborne.results.sello, docs_osborne.results.ratio, docs_osborne.documentos.ruta
 from docs_osborne.documentos, docs_osborne.results
 where docs_osborne.documentos.ruta = docs_osborne.results.ruta;
