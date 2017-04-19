29cea81799536c9184ce8e8824204d3bc76711e2
Mike Desktop PC <miguelrodrigo249@gmail.com>
2017-04-19 21:26:07 +0200

    Line separator tested and working. Position test testing pending

--- SellosHeuristica.py
+++ SellosHeuristica.py
@@ -90,6 +90,40 @@ class LineSeparator:
         return np.convolve(m[::-1], y, mode='valid')
 
     @staticmethod
+    def is_outlier(points, thresh=2.0035):
+        """
+        Returns a boolean array with True if points are outliers and False 
+        otherwise.
+
+        Parameters:
+        -----------
+            points : An numobservations by numdimensions array of observations
+            thresh : The modified z-score to use as a threshold. Observations with
+                a modified z-score (based on the median absolute deviation) greater
+                than this value will be classified as outliers.
+
+        Returns:
+        --------
+            mask : A numobservations-length boolean array.
+
+        References:
+        ----------
+            Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+            Handle Outliers", The ASQC Basic References in Quality Control:
+            Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+        """
+        if len(points.shape) == 1:
+            points = points[:, None]
+        median = np.median(points, axis=0)
+        diff = np.sum((points - median) ** 2, axis=-1)
+        diff = np.sqrt(diff)
+        med_abs_deviation = np.median(diff)
+
+        modified_z_score = 0.6745 * diff / med_abs_deviation
+
+        return modified_z_score > thresh
+
+    @staticmethod
     def find_min(a):
         """
         Encontrar el mínimo valor que se encuentre. Si entre mínimo y mínimo hay menos de, digamos 60px, se ignora el
--- test_sep_lineas.py
+++ test_sep_lineas.py
@@ -11,35 +11,42 @@ img = cv2.imread(path+'/1883-L119.M29/11/IMG_0001.png', cv2.IMREAD_GRAYSCALE)
 otsu_thresh, bin_img = cv2.threshold(img, 0, 1, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 bin_blurred = cv2.GaussianBlur(bin_img, (29, 29), 0)
 
-hist = LiSe.horiz_proy(bin_img)
+hist = LiSe.horiz_proy(bin_blurred)
 plt.plot(hist, 'b')
 
 smooth_hist = LiSe.savitzky_golay(hist, 51, 3)
 # plt.plot(smooth_hist, 'r')
 
-
 mins = LiSe.find_min(smooth_hist)
 min_x = []
 min_y = []
-min_sum = 0
 for m in mins:
-    min_sum += m[1]
     min_x.append(m[0])
     min_y.append(m[1])
-average = min_sum / len(mins)
 plt.plot(min_x, min_y, '.g')
+
+is_out = LiSe.is_outlier(np.array(min_y))
+outliers_x = []
+outliers_y = []
+for i, x in enumerate(is_out):
+    if x:
+        if hist[min_x[i]] > 0:
+            outliers_x.append(min_x[i])
+            outliers_y.append(min_y[i])
+        else:
+            is_out[i] = False
+plt.plot(outliers_x, outliers_y, '.r')
 
 # hist2 = LiSe.horiz_proy(bin_blurred)
 # plt.plot(hist2, 'r')
 
-plt.show()
-# cv2.namedWindow('Imagen', cv2.WINDOW_NORMAL)
-# cv2.imshow('Imagen', bin_img*255)
+for i, p in enumerate(min_x):
+    if not is_out[i]:
+        cv2.line(bin_img, (0, p), (3680, p), 1, 10)
+
+cv2.namedWindow('Imagen', cv2.WINDOW_NORMAL)
+cv2.imshow('Imagen', bin_img*255)
 # cv2.namedWindow('Imagen2', cv2.WINDOW_NORMAL)
 # cv2.imshow('Imagen2', bin_blurred*255)
-# cv2.waitKey()
-
-
-# TODO: por aqí va la cosa
-# TODO: usar http://stackoverflow.com/questions/4624970/finding-local-maxima-minima-with-numpy-in-a-1d-numpy-array
-# TODO: Una vez encontrados los mínimos locales, dibujar línea para ver si se detectan bien las líneas
+plt.show()
+cv2.waitKey()
